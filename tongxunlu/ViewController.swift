57832002bce42d077c8ae7d8268b2d1924a1a7da
18404715033 <965630447@qq.com>
2016-07-01 15:52:01 +0800

    通讯录
    初步连接到数据库，可以存一个号码 不能多次存和读。

--- tongxunlu/ViewController.swift
+++ tongxunlu/ViewController.swift
@@ -9,11 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var db:SQLiteDB!
+    
+    @IBOutlet weak var txtUname: UITextField!
+    
+    @IBOutlet weak var txtMobile: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        db = SQLiteDB.sharedInstance()
+        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        initUser()
     }
+    @IBAction func saveClicked(sender: AnyObject) {
+        saveUser()
+    }
+    
+    func initUser() {
+        let data = db.query("select * from t_user")
+        if data.count > 0 {
+            //获取最后一行数据显示
+            let user = data[data.count - 1]
+            txtUname.text = user["uname"] as? String
+            txtMobile.text = user["mobile"] as? String
+        }
+    }
+
+    
+    func saveUser() {
+        let uname = self.txtUname.text!
+        let mobile = self.txtMobile.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
